ac132ba77f51fdd97a5477ed3ccb79c86f0467d2
Lucas Marcos <marcos94lucas@gmail.com>
2022-06-08 14:42:05 -0300

    testes cfg

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,15 @@
-import main from "../src/main.js";
+import { irpf } from "../src/main.js";
 
-test("main working", () => {
-  expect(true).toBe(true);
+test("todos os nos", () => {
+	expect(irpf(900.00)).toBe(0.0);
+	expect(irpf(2902.00)).toBe(80.50);
+	expect(irpf(3500.00)).toBe(170.20);
+	expect(irpf(4900.00)).toBe(479.14);
+});
+
+test("todos as arestas", () => {
+	expect(irpf(900.00)).toBe(0.0);
+	expect(irpf(2902.00)).toBe(80.50);
+	expect(irpf(3500.00)).toBe(170.20);
+	expect(irpf(4900.00)).toBe(479.14);
 });
